c4db9dc6c8c595a5e778d7ea7ea63f88ae31009a
Roman Khodosov <roma.pro.khodosov@gmail.com>
2026-04-04 17:48:20 +0300

    пкрший комміт

---
+++ task-3.js
@@ -0,0 +1,43 @@
+// Завдання 3
+// Напиши клас Storage, який буде створювати об'єкти для управління складом товарів. При виклику буде отримувати один аргумент -
+//  початковий масив товарів, і записувати його у властивість items.
+// Додай методи класу:
+// getItems() — повертає масив поточних товарів
+// addItem(item) — отримує новий товар і додає його до поточних
+// removeItem(item) — отримує товар і, якщо він є, видаляє його з поточних
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
+  getItems() {
+    return this.items;
+  }
+  addItem(item) {
+    items.push(item);
+    return this.items;
+  }
+  removeItem(item) {
+    const indexOfItem = this.items.indexOf(item);
+    if (items.includes(item)) {
+      items.splice(indexOfItem, 1);
+      return this.items;
+    } else {
+      console.log("такого товару немає");
+    }
+  }
+}
+const storage = new Storage([
+  "Нанітоіди",
+  "Пролонгер",
+  "Залізні жупи",
+  "Антигравітатор",
+]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанітоіди", "Пролонгер", "Залізні жупи", "Антигравітатор" ]
+
+storage.addItem("Дроїд");
+console.table(storage.items); // [ "Нанітоіди", "Пролонгер", "Залізні жупи", "Антигравітатор", "Дроїд" ]
+
+storage.removeItem("Пролонгер");
+console.table(storage.items); // [ "Нанітоіди", "Залізні жупи", "Антигравітатор", "Дроїд" ]
---
+++ task-5.js
@@ -0,0 +1,64 @@
+// Завдання 5
+// Напиши клас Car із зазначеними властивостями і методами.
+class Car {
+  constructor({ maxSpeed, price, speed = 0, isOn = false, distance = 0 }) {
+    this.maxSpeed = maxSpeed;
+    this._price = price;
+    this.speed = speed;
+    this.isOn = isOn;
+    this.distance = distance;
+  }
+  static getSpecs(car) {
+    console.log(
+      `maxSpeed: ${car.maxSpeed}, speed: ${car.speed}, isOn: ${car.isOn}, distance: ${car.distance}, price: ${car.price}`,
+    );
+  }
+  get price() {
+    return this._price;
+  }
+  set price(newPrice) {
+    this._price = newPrice;
+  }
+  turnOn() {
+    this.isOn = true;
+  }
+  turnOff() {
+    this.isOn = false;
+    this.speed = 0;
+  }
+  accelerate(value) {
+    if (this.speed + value <= this.maxSpeed) {
+      this.speed += value;
+    }
+  }
+  decelerate(value) {
+    if (this.speed - value >= 0) {
+      this.speed -= value;
+    }
+  }
+  drive(hours) {
+    if (this.isOn) {
+      this.distance += this.speed * hours;
+    }
+  }
+}
+
+const mustang = new Car({ maxSpeed: 200, price: 2000 });
+
+mustang.turnOn();
+mustang.accelerate(50);
+mustang.drive(2);
+
+Car.getSpecs(mustang);
+// maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
+
+mustang.decelerate(20);
+mustang.drive(1);
+mustang.turnOff();
+
+Car.getSpecs(mustang);
+// maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000
+
+console.log(mustang.price); // 2000
+mustang.price = 4000;
+console.log(mustang.price); // 4000
